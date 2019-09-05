d8e1f9cffa67b70a0a9f32615b5c53fed123af6d
Romain Freydiger <freydiger.r@sfeir.com>
2019-09-05 15:38:22 +0200

    interface statement

---
+++ client/src/ts/interfaces/done.ts
@@ -0,0 +1,4 @@
+/**
+ * TODO: Create an interface "Done" which extends "TaskProperties"
+ * This interface will have a property state which is a string
+ */
---
+++ client/src/ts/interfaces/in-progress.ts
@@ -0,0 +1,4 @@
+/**
+ * TODO: Create an interface "Todo" which extends "TaskProperties"
+ * This interface will have a property state which is a string (status "in progress")
+ */
