7ecf24227652d2c754ce9e0e6700ed2f8e9d312b
Romain Freydiger <freydiger.r@sfeir.com>
2019-09-05 15:44:33 +0200

    generic statement

--- client/src/ts/todo-service.ts
+++ client/src/ts/todo-service.ts
@@ -2,18 +2,21 @@ export class TodoService {
 
   private apiUrl = 'http://localhost:9000/api';
 
+  // TODO: function -> return JQuery.jqXHR of BackendResponse of Array of Todo
   findAll() {
     return $.ajax({
       url: `${this.apiUrl}/all`
     });
   }
 
+  // TODO: function -> return JQuery.jqXHR of BackendResponse of Array of Task
   findMine() {
     return $.ajax({
       url: `${this.apiUrl}/my`
     });
   }
 
+  // TODO: function -> return JQuery.jqXHR of void
   delete(id) {
     return $.ajax({
       method: 'DELETE',
@@ -21,6 +24,7 @@ export class TodoService {
     });
   }
 
+  // TODO: function -> return JQuery.jqXHR of void
   toInProgress(id) {
     return $.ajax({
       method: 'PUT',
@@ -28,6 +32,7 @@ export class TodoService {
     });
   }
 
+  // TODO: function -> return JQuery.jqXHR of void
   toDone(id){
     return $.ajax({
       method: 'PUT',
@@ -35,6 +40,7 @@ export class TodoService {
     });
   }
 
+  // TODO: function -> return JQuery.jqXHR of BackendResponse of Todo
   create(task) {
     return $.ajax({
       method: 'PUT',
