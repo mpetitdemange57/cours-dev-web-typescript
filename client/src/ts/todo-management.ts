823cac54a1445005e896b39e78ae994ba34d7a44
Romain Freydiger <freydiger.r@sfeir.com>
2019-09-05 10:59:01 +0200

    class statement

--- client/src/ts/todo-management.ts
+++ client/src/ts/todo-management.ts
@@ -6,12 +6,16 @@ import { generateTodoTile } from './generate-todo-tile';
 export class TodoManagement {
   todoList = [];
 
+  // TODO: JQuery
   private readonly inProgressContainer = $('#todo-list-in-progress');
 
+  // TODO: JQuery
   private toDoContainer = $('#todo-list-todo');
 
+  // TODO: JQuery
   private readonly doneContainer = $('#todo-list-done');
 
+  // TODO: todoService -> TodoService
   constructor(private todoService) {
     todoService.findMine().then(response => {
       this.todoList = response.data;
@@ -31,22 +35,26 @@ export class TodoManagement {
     })
   }
 
+  // TODO: function -> return nothing
   clearAll() {
     this.clearList(this.toDoContainer);
     this.clearList(this.inProgressContainer);
     this.clearList(this.doneContainer);
   }
 
+  // TODO: list -> JQuery, function -> return nothing
   clearList(list) {
     list.find('.todo-tile').remove();
   }
 
+  // TODO: function -> return nothing
   drawAll() {
     this.drawList(this.toDoContainer, TodoStatus.toDo);
     this.drawList(this.inProgressContainer, TodoStatus.inProgress);
     this.drawList(this.doneContainer, TodoStatus.done);
   }
 
+  // TODO: list: JQuery, state -> string, function -> return nothing
   drawList(list, state) {
     this.todoList
     .filter(todo => todo.state === state)
@@ -57,6 +65,7 @@ export class TodoManagement {
     });
   }
 
+  // TODO: newTodoData -> Todo, function -> return nothing
   create(newTodoData) {
     this.todoService.create(newTodoData)
       .then((response) => {
@@ -67,6 +76,7 @@ export class TodoManagement {
       });
   }
 
+  // TODO: todoTile: JQuery, function -> return nothing (todo typed later)
   progressTodo(todoTile, todo) {
     let listToAppend;
     let promise;
