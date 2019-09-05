09ef2d67e2e25eb519625a05fcee273f6d65d375
Romain Freydiger <freydiger.r@sfeir.com>
2019-09-05 15:44:33 +0200

    generic statement

---
+++ client/src/ts/interfaces/backend-response.ts
@@ -0,0 +1,5 @@
+/**
+ * TODO: create a generic interface "BackendResponse" that represent the format of data returned by the backend. It must have properties :
+ * - data which is type of the generic provided
+ * - metadata which is an object with a property "url" of type string
+ */
